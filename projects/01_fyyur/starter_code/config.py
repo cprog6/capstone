e853e6c17fd080a730def7322182c9a641fb71fa
Rick Snyder <cprog6@yahoo.com>
2021-06-25 09:36:15 -0500

    Changes again

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -5,3 +5,28 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Connect to the database
 
+# TODO IMPLEMENT DATABASE URL
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = os.urandom(32)
+    SQLALCHEMY_DATABASE_URI = 'postgres+psycopg2://postgres:Theoilers1#tgres@127.0.0.1:5432/rickdb'
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
